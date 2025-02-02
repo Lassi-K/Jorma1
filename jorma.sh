6547b40ad0b373057170c76aa30445f8daae5457
Lazi <jotain.seppo@hotmail.com>
2025-02-02 09:54:10 +0200

    jotaqin

--- jorma.sh
+++ jorma.sh
@@ -18,9 +18,6 @@ echo -n ">> "
 read input
 
 
-while [[ $run == "True" ]];
-do
-
 while [[ $paikka == "ullakko" ]];
 do
 
@@ -29,39 +26,37 @@ do
     		echo "Olet autiotalon ullakkohuoneessa"
     		echo "Lattialla lojuu pesäpallomaila"
     		echo "Huoneen toisella puolella on ikkuna ja toisella ovi"
-
     		echo -n ">> "
     		read input
 
 	
 	elif [[ $input == "mene ikkunalle" ]]; then
 		echo "Putoat ikkunasta ja kuolet"
-		kill $$
+		break
 		
 
 	elif [[ $input == "ota maila" || $input == "ota pesäpallomaila" ]]; then
     		unset input
     		echo "Maila otettu"
     		maila=true
-
 		echo -n ">> "
     		read input
+
 
 	elif [[ $input == "mene ovelle" && $ovella == "false" || $input == "mene oven luo" && $ovella == "false" ]]; then
 		unset input
 		echo "Kävellään..."
 		sleep 2
 		echo "Ovella"
-		
 		echo -n ">> "
 		read input
 		ovella=true
+
 
 	elif [[ $input == "katso ovea" && $ovella == "true" ]]; then
 		unset input
 		echo "Se on vanha ja tehty puusta"
 		echo "Lukko on rikki"
-		
 		echo -n ">> "
 		read input
 	
@@ -69,7 +64,6 @@ do
 	elif [[ $input == "käytä mailaa oveen" && $ovella == "true" && $maila == "true" && $rikki == "false" || $input == "lyö ovea mailalla" && $ovella == "true" && $maila == "true" && $rikki == "false" ]]; then
 		
 		rikki=true	
-
 		unset input
 		echo "Ovi hajosi"
 		echo -n ">> "
@@ -80,18 +74,15 @@ do
 		unset maila
 		unset ovella
 		unset rikki
-		
 		unset input
 		echo "Olet pimeässä portaikossa"
-		
 		paikka=portaikko
-		
 		echo -n ">> "
 		read input
 		
+
 	else
 		echo "Ma ei ummarrra"
-		
 		unset input
 		echo -n ">> "
 		read input
@@ -102,24 +93,17 @@ while [[ $paikka == "portaikko" ]];
 do
 	if [[ $input == "katso ympärillesi" ]]; then
 		unset input
-		
 		echo "Olet pimeässä portaikossa"
-
 		echo -n ">> "
 		read input
 
 	
 	elif [[ $input == "mene alas" ]]; then
-
 		paikka=alhaalla
 		unset input
-
 		echo "Kävellään..."
-		
 		sleep 2
-
 		echo "Alhaalla"
-
 		echo -n ">> "
 		read input
 
@@ -128,7 +112,6 @@ do
 	else
 		echo "Ma ei ummarra"
 		unset input
-
 		echo -n ">> "
 		read input
 
@@ -141,40 +124,32 @@ do
 
 	if [[ $input == "katso ympärillesi" ]]; then
 		unset input
-
 		echo "Olet alakerrassa ja siellä on pimeää"
-
 		echo -n ">> "
 		read input
 
 
 	elif [[ $input == "mene eteenpäin" ]]; then
 		unset input
-
 		tappelu=true
-
 		echo "Törmäät isoon vihreään otukseen ja sillä on veitsi kädessä!"
 		echo "Mitä teet!?"
-
 		echo -n ">> "
 		read input
 
-	
+
 	elif [[ $input == "lyö pesäpallomailalla otusta" && $tappelu == "true" || $input == "lyö mailalla otusta" && $tappelu == "true"|| $input == "lyö mailalla" && $tappelu == "true" ]]; then
 		unset input
-
 		echo "Otus kaatui ja kuoli"
 		echo "Voitit pelin!"
-
-		kill $$
+		break
 	
+
 	elif [[ $input == "juokse karkuun" || $input == "mene karkuun" || $input == "juokse" ]]; then
 		unset input
-
 		echo "Otus lyö sinua veitsellänsä ja kuolet"
 		echo "Aloita alusta, jos haluat voittaa pelin"
-
-		kill $$
+		break
 
 
 	else
@@ -182,9 +157,6 @@ do
 		unset input
 		echo -n ">> "
 		read input
-
-
 	fi
 done
 
-done
